77d0cabf1fef265991e1af1f5fecb0e01cdfb236
NathanCvl <nathancuvelier59@icloud.com>
2022-10-19 15:55:43 +0200

    docs(commons): add gitignore and js file

--- triangle-loop/looping-triangle.js
+++ triangle-loop/looping-triangle.js
@@ -1,11 +1,4 @@
 "use strict"
-function looping(n) {
-    let arr = [];
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= i; j++) {
-            arr.push("#");
-            console.log(arr);
-        }
-        console.log("\n");
-    }
+function loopin() {
+    
 }
